8cbeab2851c296cda96392dbaa1692dd2fd7d446
Ryan Morgan <rrgy80@gmail.com>
2019-10-03 09:11:28 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.dropdown = React.createRef()
+  }
+
+  toggleContainer = () => {
+    console.log(this.dropdown)
+    // let { current } = this.dropdown
+    // if (current.classList.contains('show-animation')) {
+    //   current.classList.add('hide-animation')
+    //   current.classList.remove('show-animation')
+    // } else {
+    //   current.classList.add('show-animation')
+    //   current.classList.remove('hide-animation')
+    // }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="image">
+          <header>
+            <div className='header-bar'>
+              <h2 className='bootstrap'>Start Bootstrap</h2>
+              <button className='menuButton' onClick={this.toggleContainer}> MENU
+                <i className='fas fa-bars'></i>
+              </button>
+              <div className='container'>
+                <div className='buttons'>Services</div>
+                <div className='buttons'>Portfolio</div>
+                <div className='buttons'>About</div>
+                <div className='buttons'>Team</div>
+                <div className='buttons'>Contact</div>
+              </div>
+            </div>
+          </header>
+          <div className='container-dropdown' ref={this.dropdown}>
+            <div className='dropdown-buttons'>Services</div>
+            <div className='dropdown-buttons'>Portfolio</div>
+            <div className='dropdown-buttons'>About</div>
+            <div className='dropdown-buttons'>Team</div>
+            <div className='dropdown-buttons'>Contact</div>
+          </div>
+          <h1>WelcomeTo Our Studio!</h1>
+          <h1 className='meet-you'>IT'S NICE TO MEET YOU</h1>
+          <div className='more-button'>TELL US MORE</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
